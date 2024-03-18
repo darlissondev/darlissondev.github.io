5dc31454efd73256db5d7a4415e7af8c4ebd8c7c
Darlisson Limeira <darlisson.dev@gmail.com>
2024-03-18 18:17:38 -0300

    improve file documentation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,14 @@
+// pega elementos do DOM
 const closeMenuBtn = document.querySelector(".close-menu-btn");
 const menuBtn = document.querySelector(".menu-btn");
 const navBar = document.querySelector(".navbar");
 
-// Funcao para abrir e fechar o menu mobile
+// Evento para abrir e fechar o menu mobile
 const toggleMenu = () => {
   navBar.classList.toggle("show-navbar");
   closeMenuBtn.classList.toggle("show-close-menu-btn");
 };
 
-// Adicionar o evento ao botoes de abrir e fechar o menu mobile
+// Adiciona o toggleMenu ao botao menuBtn e closeMenuBtn
 menuBtn.addEventListener("click", toggleMenu);
 closeMenuBtn.addEventListener("click", toggleMenu);
